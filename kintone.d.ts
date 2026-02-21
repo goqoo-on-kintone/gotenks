244f815df3fb1a6cd11e25bdd7d539a1f472d8f5
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2026-02-21 19:20:21 +0900

    chore: dtsファイルの型エラーが出ないように対応

---
+++ kintone.d.ts
@@ -0,0 +1,27 @@
+// kintone 型定義のスタブ（サンプルファイル用）
+declare namespace kintone.fieldTypes {
+  interface SingleLineText {}
+  interface MultiLineText {}
+  interface RichText {}
+  interface Number {}
+  interface Link {}
+  interface Date {}
+  interface Time {}
+  interface DateTime {}
+  interface DropDown {}
+  interface RadioButton {}
+  interface CheckBox {}
+  interface MultiSelect {}
+  interface Calc {}
+  interface UserSelect {}
+  interface OrganizationSelect {}
+  interface GroupSelect {}
+  interface File {}
+  interface Creator {}
+  interface Modifier {}
+  interface Id {}
+  interface Revision {}
+  interface RecordNumber {}
+  interface CreatedTime {}
+  interface UpdatedTime {}
+}
