984919c6822b39b03352bf712713d1280f8f4ac1
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2026-02-21 19:01:51 +0900

    style: 自動フォーマット

--- internal/parser/debug_test.go
+++ internal/parser/debug_test.go
@@ -10,11 +10,11 @@ func TestDebugFacilityFile(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Error: %v", err)
 	}
-	
+
 	for _, iface := range result.Interfaces {
 		fmt.Printf("Interface: %s (extends: %s)\n", iface.Name, iface.Extends)
 		for i, f := range iface.Fields {
-			fmt.Printf("  [%d] %s: type=%s, isSubtable=%v, subfields=%d\n", 
+			fmt.Printf("  [%d] %s: type=%s, isSubtable=%v, subfields=%d\n",
 				i, f.Name, f.TypeName, f.IsSubtable, len(f.SubtableFields))
 		}
 	}
